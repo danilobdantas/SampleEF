429b050e45a8d494635058651b684a70c4b3480c
Danilo de Brito Dantas <danilobdantas@gmail.com>
2020-08-11 17:15:05 -0300

    supahot

--- SampleEF/Controllers/WeatherForecastController.cs
+++ SampleEF/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SampleEF.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "SupaHot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
